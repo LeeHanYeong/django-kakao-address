2c324d67968931e2e643f191319eafd961b6594b
이한영 <lhy@tpay.co.kr>
2019-06-19 19:34:55 +0900

    Write deploy script

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python
 import os
 from setuptools import setup
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 README = open(os.path.join(ROOT_DIR, 'README.md')).read()
+VERSION = open(os.path.join(ROOT_DIR, 'version.txt')).read()
 
 setup(
     name='django-kakao-zipcode',
-    version='0.1.0',
+    version=VERSION,
     description='Kakao zipcode service widget for Django',
     long_description=README,
+    long_description_content_type='text/markdown',
     author='Lee HanYeong',
     author_email='dev@lhy.kr',
     license='MIT',
